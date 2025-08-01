e97e172408e52a9cfff4dbea8e29aaca1480b359
Matt Couch <mcouch@gmail.com>
2025-08-01 14:24:47 -0500

    wave counter

--- scripts/objects/player.gd
+++ scripts/objects/player.gd
@@ -2,13 +2,18 @@ extends StaticBody2D
 @export var health:float = 500
 
 @onready var health_bar = %Healthbar
+@onready var wave_number_label = %WaveNumberLabel
 
 func _ready():
 	health_bar.max_value = health;
 	health_bar.value = health;
+	GameStateController.started.connect(_wave_started)
 
 func take_damage(info: DamageInfo):
 	health -= info.damage;
 	health_bar.value = health;
 	#if (health <= 0):
 		#queue_free();
+
+func _wave_started():
+	wave_number_label.text = "%s" % GameStateController.waveCount
